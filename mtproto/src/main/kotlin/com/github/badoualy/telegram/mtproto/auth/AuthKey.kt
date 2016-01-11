6ba3c0824be9f81020c6b7b167e3e85bf3223f0b
Yannick Badoual <yann.badoual@gmail.com>
2016-01-11 15:53:58 +0100

    Added ByteBuffer constructor for AuthKey and security check for its length

--- mtproto/src/main/kotlin/com/github/badoualy/telegram/mtproto/auth/AuthKey.kt
+++ mtproto/src/main/kotlin/com/github/badoualy/telegram/mtproto/auth/AuthKey.kt
@@ -2,8 +2,17 @@ package com.github.badoualy.telegram.mtproto.auth
 
 import com.github.badoualy.telegram.mtproto.secure.CryptoUtils
 import java.math.BigInteger
+import java.nio.ByteBuffer
 
 open class AuthKey(val key: ByteArray) {
+
+    constructor(key: ByteBuffer) : this(key.array())
+
+    init {
+        if (key.size != 256)
+            throw RuntimeException("AuthKey must be 256 Bytes found ${key.size} bytes")
+    }
+
     // see https://core.telegram.org/mtproto/description#key-identifier
     // SHA1 generates 20 bytes long hash, authKeyId is the lower 8 bytes
     val keyId = CryptoUtils.substring(CryptoUtils.SHA1(key), 12, 8)
