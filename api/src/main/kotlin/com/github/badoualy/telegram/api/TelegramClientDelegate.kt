6d409ccac6c81d397945641b55fec320435013c5
Yannick Badoual <yann.badoual@gmail.com>
2016-01-15 16:47:21 +0100

    Fixed missing call to initConnection when not checking DC

--- api/src/main/kotlin/com/github/badoualy/telegram/api/TelegramClientDelegate.kt
+++ api/src/main/kotlin/com/github/badoualy/telegram/api/TelegramClientDelegate.kt
@@ -6,6 +6,7 @@ import com.github.badoualy.telegram.mtproto.auth.AuthKey
 import com.github.badoualy.telegram.mtproto.auth.AuthKeyCreation
 import com.github.badoualy.telegram.mtproto.exception.RpcErrorException
 import com.github.badoualy.telegram.mtproto.util.Log
+import com.github.badoualy.telegram.tl.api.TLNearestDc
 import com.github.badoualy.telegram.tl.api.requests.TLRequestHelpGetNearestDc
 import com.github.badoualy.telegram.tl.api.requests.TLRequestInitConnection
 import com.github.badoualy.telegram.tl.api.requests.TLRequestInvokeWithLayer18
@@ -52,8 +53,21 @@ internal class TelegramClientDelegateImpl(val application: TelegramApp, val apiS
     private fun init(attemptCount: Int = 0, checkNearestDc: Boolean = true) {
         mtProtoHandler = if (generateAuthKey) initWithoutKey() else MTProtoHandler(dataCenter!!, authKey!!)
         mtProtoHandler!!.startWatchdog()
-        if (checkNearestDc)
-            checkNearestDc(attemptCount)
+
+        try {
+            // Call to initConnection to setup information about this app for the user to see in "active sessions"
+            // Also will indicate to Telegram that we're using layer 18
+            val nearestDc = mtProtoHandler!!
+                    .executeMethod(TLRequestInvokeWithLayer18(TLRequestInitConnection(application.apiId, application.deviceModel, application.systemVersion, application.appVersion, application.langCode, TLRequestHelpGetNearestDc())))
+                    .toBlocking().first()
+            if (checkNearestDc)
+                checkNearestDc(nearestDc, attemptCount)
+        } catch(e: IOException) {
+            mtProtoHandler?.close()
+            if (e is RpcErrorException && e.error.errorCode == -404)
+                throw RuntimeException("Your authorization key seems to be invalid (error " + e.error.errorCode + ")")
+            throw RuntimeException("An unknown error has occurred", e)
+        }
     }
 
     private fun initWithoutKey(): MTProtoHandler {
@@ -68,45 +82,30 @@ internal class TelegramClientDelegateImpl(val application: TelegramApp, val apiS
         }
     }
 
-    private fun checkNearestDc(attemptCount: Int) {
-        // Call to initConnection to setup information about this app for the user to see in "active sessions"
-        // Also will indicate to Telegram that we're using layer 18
-        try {
-            // List all DC
-            //val config = invokeWithLayer18_(TLRequestInitConnection(application.apiId, application.deviceModel, application.systemVersion, application.appVersion, application.langCode, TLRequestHelpGetConfig())) as TLConfig
-            //for (dc in config.dcOptions) Log.d(TAG, "DC${dc.id} at ${dc.ipAddress}:${dc.port} with hostname ${dc.hostname}")
-
-            val nearestDc = mtProtoHandler!!
-                    .executeMethod(TLRequestInvokeWithLayer18(TLRequestInitConnection(application.apiId, application.deviceModel, application.systemVersion, application.appVersion, application.langCode, TLRequestHelpGetNearestDc())))
-                    .toBlocking().first()
-            if (nearestDc.thisDc != nearestDc.nearestDc) {
-                mtProtoHandler?.close()
-                Log.d(TAG, "Current DC${nearestDc.thisDc} is not the nearest, connecting to DC${nearestDc.nearestDc}")
-
-                if (attemptCount > 0)
-                    throw RuntimeException("TLRequestHelpGetNearestDc returned inconsistent values: got two different values for two consecutive calls")
-
-                if (!generateAuthKey) {
-                    // Key was provided, yet selected DC is not the nearest
-                    // TODO: Should handle authKey migration via auth.exportAuthorization
-                    throw RuntimeException("You tried to connect to an incorrect data center (DC${nearestDc.thisDc}) with an authorization key, please connect to the nearest (DC${nearestDc.nearestDc})")
-                } else {
-                    // We have to re-open connection to new dc
-                    authKey = null
-                    dataCenter = Kotlogram.PROD_DCS[nearestDc.nearestDc - 1]
-                    apiStorage.deleteAuthKey()
-                    Log.d(TAG, "Updated dataCenter to DC${nearestDc.nearestDc} ${dataCenter.toString()}")
-                    generateAuthKey = true
-                    init(attemptCount + 1)
-                }
-            } else {
-                Log.d(TAG, "Connected to the nearest DC${nearestDc.thisDc}")
-            }
-        } catch(e: IOException) {
+    @Throws(IOException::class)
+    private fun checkNearestDc(nearestDc: TLNearestDc, attemptCount: Int) {
+        if (nearestDc.thisDc != nearestDc.nearestDc) {
             mtProtoHandler?.close()
-            if (e is RpcErrorException && e.error.errorCode == -404)
-                throw RuntimeException("Your authorization key seems to be invalid (error " + e.error.errorCode + ")")
-            throw RuntimeException("An unknown error has occurred", e)
+            Log.d(TAG, "Current DC${nearestDc.thisDc} is not the nearest, connecting to DC${nearestDc.nearestDc}")
+
+            if (attemptCount > 0)
+                throw RuntimeException("TLRequestHelpGetNearestDc returned inconsistent values: got two different values for two consecutive calls")
+
+            if (!generateAuthKey) {
+                // Key was provided, yet selected DC is not the nearest
+                // TODO: Should handle authKey migration via auth.exportAuthorization
+                throw RuntimeException("You tried to connect to an incorrect data center (DC${nearestDc.thisDc}) with an authorization key, please connect to the nearest (DC${nearestDc.nearestDc})")
+            } else {
+                // We have to re-open connection to new dc
+                authKey = null
+                dataCenter = Kotlogram.PROD_DCS[nearestDc.nearestDc - 1]
+                apiStorage.deleteAuthKey()
+                Log.d(TAG, "Updated dataCenter to DC${nearestDc.nearestDc} ${dataCenter.toString()}")
+                generateAuthKey = true
+                init(attemptCount + 1)
+            }
+        } else {
+            Log.d(TAG, "Connected to the nearest DC${nearestDc.thisDc}")
         }
     }
 
