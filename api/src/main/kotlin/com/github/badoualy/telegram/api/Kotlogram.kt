cc31849baae660453aa56d3e9e7b534bea2d7111
Yannick Badoual <yann.badoual@gmail.com>
2016-07-14 15:52:47 +0200

    Put api layer field as JvmField

--- api/src/main/kotlin/com/github/badoualy/telegram/api/Kotlogram.kt
+++ api/src/main/kotlin/com/github/badoualy/telegram/api/Kotlogram.kt
@@ -6,8 +6,9 @@ import org.slf4j.LoggerFactory
 
 object Kotlogram {
 
-    val logger = LoggerFactory.getLogger(Kotlogram::class.java)
+    val logger = LoggerFactory.getLogger(Kotlogram::class.java)!!
 
+    @JvmField
     val API_LAYER = 53
 
     init {
@@ -36,13 +37,13 @@ object Kotlogram {
         logger.warn("==================== CLEANED ====================")
     }
 
-     @JvmField val PROD_DC1 = DataCenter("149.154.175.50", 443)
-     @JvmField val PROD_DC2 = DataCenter("149.154.167.51", 443)
-     @JvmField val PROD_DC3 = DataCenter("149.154.175.100", 443)
-     @JvmField val PROD_DC4 = DataCenter("149.154.167.91", 443)
-     @JvmField val PROD_DC5 = DataCenter("91.108.56.165", 443)
+    @JvmField val PROD_DC1 = DataCenter("149.154.175.50", 443)
+    @JvmField val PROD_DC2 = DataCenter("149.154.167.51", 443)
+    @JvmField val PROD_DC3 = DataCenter("149.154.175.100", 443)
+    @JvmField val PROD_DC4 = DataCenter("149.154.167.91", 443)
+    @JvmField val PROD_DC5 = DataCenter("91.108.56.165", 443)
 
-     @JvmField val PROD_DCS = arrayOf(PROD_DC1, PROD_DC2, PROD_DC3, PROD_DC4, PROD_DC5)
+    @JvmField val PROD_DCS = arrayOf(PROD_DC1, PROD_DC2, PROD_DC3, PROD_DC4, PROD_DC5)
 
     @JvmStatic
     fun getDcById(id: Int) = PROD_DCS[id - 1]
