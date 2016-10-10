f46a3a86ebd04942e08d3aee1ce106b5792dd9e4
Yannick Badoual <yann.badoual@gmail.com>
2016-10-10 08:15:12 +0200

    Removed unused constructor (in this new layer) Regenerate dump Disable SendSticker sample for now

--- sample/src/main/java/com/github/badoualy/telegram/sample/SampleSendSticker.java
+++ sample/src/main/java/com/github/badoualy/telegram/sample/SampleSendSticker.java
@@ -3,24 +3,20 @@ package com.github.badoualy.telegram.sample;
 import com.github.badoualy.telegram.api.Kotlogram;
 import com.github.badoualy.telegram.api.TelegramClient;
 import com.github.badoualy.telegram.tl.api.TLAbsInputPeer;
-import com.github.badoualy.telegram.tl.api.TLAbsUpdates;
-import com.github.badoualy.telegram.tl.api.TLDocument;
-import com.github.badoualy.telegram.tl.api.TLInputDocument;
-import com.github.badoualy.telegram.tl.api.TLInputMediaDocument;
 import com.github.badoualy.telegram.tl.api.TLInputPeerEmpty;
+import com.github.badoualy.telegram.tl.api.TLStickerSet;
 import com.github.badoualy.telegram.tl.api.messages.TLAbsDialogs;
-import com.github.badoualy.telegram.tl.api.messages.TLStickers;
+import com.github.badoualy.telegram.tl.api.messages.TLAllStickers;
 import com.github.badoualy.telegram.tl.exception.RpcErrorException;
 
 import java.io.IOException;
-import java.util.Random;
 
 import static com.github.badoualy.telegram.sample.C.ApiStorage;
 import static com.github.badoualy.telegram.sample.C.application;
 import static com.github.badoualy.telegram.sample.SampleGetHistory.getInputPeer;
 
 /**
- * This snippet will get the most recent conversation and send a sticker in thie conversation.
+ * This snippet will get the most recent conversation and send a sticker in this conversation.
  */
 public class SampleSendSticker {
 
@@ -33,9 +29,10 @@ public class SampleSendSticker {
             TLAbsDialogs tlAbsDialogs = client.messagesGetDialogs(0, 0, new TLInputPeerEmpty(), 1);
             TLAbsInputPeer inputPeer = getInputPeer(tlAbsDialogs);
 
-            // Get the stickers available for emoji sunglass
-            TLStickers tlStickers = (TLStickers) client.messagesGetStickers("\uD83D\uDE0E", "");
-            if (!tlStickers.getStickers().isEmpty()) {
+            TLAllStickers tlAllStickers = (TLAllStickers) client.messagesGetAllStickers(0);
+            TLStickerSet tlStickerSet = tlAllStickers.getSets().get(0);
+            // TODO fix
+            /*if (!tlStickers.getStickers().isEmpty()) {
                 // Take first available one
                 TLDocument tlDocument = tlStickers.getStickers().get(0).getAsDocument();
                 TLInputDocument tlInputDocument = new TLInputDocument(tlDocument.getId(), tlDocument.getAccessHash());
@@ -46,7 +43,7 @@ public class SampleSendSticker {
                 // tlAbsUpdates contains the id and date of the message in a TLUpdateShortSentMessage
             } else {
                 System.err.println("No sticker found");
-            }
+            }*/
         } catch (RpcErrorException | IOException e) {
             e.printStackTrace();
         } finally {
