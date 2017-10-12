d055e9e69481b68d4e9a7f3377a3dad7683c0564
Yann Badoual <yann.badoual@gmail.com>
2017-10-12 21:30:27 +0200

    Add methods in sync client

--- api/src/main/kotlin/com/github/badoualy/telegram/api/TelegramClient.kt
+++ api/src/main/kotlin/com/github/badoualy/telegram/api/TelegramClient.kt
@@ -7,8 +7,6 @@ import com.github.badoualy.telegram.tl.api.TLAbsInputPeer
 import com.github.badoualy.telegram.tl.api.TelegramApi
 import com.github.badoualy.telegram.tl.api.TelegramApiWrapper
 import com.github.badoualy.telegram.tl.api.account.TLPassword
-import com.github.badoualy.telegram.tl.api.auth.TLAuthorization
-import com.github.badoualy.telegram.tl.api.auth.TLSentCode
 import com.github.badoualy.telegram.tl.core.TLBytes
 import com.github.badoualy.telegram.tl.core.TLMethod
 import com.github.badoualy.telegram.tl.core.TLObject
@@ -75,11 +73,11 @@ abstract class TelegramClient : TelegramApiWrapper(), TelegramApi, Closeable {
 
     @Deprecated("Use one of the overload for more convenience",
                 ReplaceWith("authSendCode(phoneNumber)"))
-    override fun authSendCode(allowFlashcall: Boolean, phoneNumber: String, currentNumber: Boolean?, apiId: Int, apiHash: String): Single<TLSentCode> =
+    override fun authSendCode(allowFlashcall: Boolean, phoneNumber: String, currentNumber: Boolean?, apiId: Int, apiHash: String) =
             super.authSendCode(allowFlashcall, phoneNumber, currentNumber, apiId, apiHash)
 
     /** Convenience method wrapping the argument with salt */
-    fun authCheckPassword(password: String): Single<TLAuthorization> =
+    fun authCheckPassword(password: String) =
             accountGetPassword()
                     .flatMap { tlPassword ->
                         tlPassword as? TLPassword ?: throw RpcErrorException(400, "NO_PASSWORD")
@@ -92,8 +90,7 @@ abstract class TelegramClient : TelegramApiWrapper(), TelegramApi, Closeable {
 
     @Deprecated("Use authCheckPassword for more convenience",
                 ReplaceWith("authCheckPassword(password)"))
-    override fun authCheckPassword(passwordHash: TLBytes): Single<TLAuthorization> =
-            super.authCheckPassword(passwordHash)
+    override fun authCheckPassword(passwordHash: TLBytes) = super.authCheckPassword(passwordHash)
 
     /** Convenience method wrapping the argument with TelegramApp values and casting result with good type */
     fun <T : TLObject> initConnection(query: TLMethod<T>) = with(app) {
@@ -111,7 +108,7 @@ abstract class TelegramClient : TelegramApiWrapper(), TelegramApi, Closeable {
                                                systemLangCode: String,
                                                langPack: String,
                                                langCode: String,
-                                               query: TLMethod<T>?): Single<T> =
+                                               query: TLMethod<T>?) =
             super.initConnection(apiId, deviceModel, systemVersion, appVersion,
                                  systemLangCode, langPack, langCode, query)
 
--- api/src/main/kotlin/com/github/badoualy/telegram/api/TelegramSyncClient.kt
+++ api/src/main/kotlin/com/github/badoualy/telegram/api/TelegramSyncClient.kt
@@ -1,6 +1,11 @@
 package com.github.badoualy.telegram.api
 
+import com.github.badoualy.telegram.mtproto.secure.CryptoUtils
+import com.github.badoualy.telegram.tl.api.TLAbsInputPeer
 import com.github.badoualy.telegram.tl.api.TelegramSyncApiWrapper
+import com.github.badoualy.telegram.tl.api.account.TLPassword
+import com.github.badoualy.telegram.tl.api.auth.TLAuthorization
+import com.github.badoualy.telegram.tl.core.TLBytes
 import com.github.badoualy.telegram.tl.core.TLMethod
 import com.github.badoualy.telegram.tl.core.TLObject
 import com.github.badoualy.telegram.tl.exception.RpcErrorException
@@ -9,7 +14,63 @@ import com.github.badoualy.telegram.tl.exception.RpcErrorException
  * A Telegram client to make synchronous rpc calls.
  */
 abstract class TelegramSyncClient : TelegramSyncApiWrapper() {
-    // TODO: add convenience methods
+
+    abstract val app: TelegramApp
+
+    /** Convenience method wrapping the argument with [TelegramApp] values and without flashcall */
+    fun authSendCode(phoneNumber: String) = authSendCode(false, phoneNumber, null)
+
+    /** Convenience method wrapping the argument with [TelegramApp] values */
+    fun authSendCode(allowFlashcall: Boolean, phoneNumber: String, currentNumber: Boolean?) =
+            with(app) {
+                @Suppress("DEPRECATION")
+                authSendCode(allowFlashcall, phoneNumber, currentNumber, apiId, apiHash)
+            }
+
+    @Deprecated("Use one of the overload for more convenience",
+                ReplaceWith("authSendCode(phoneNumber)"))
+    override fun authSendCode(allowFlashcall: Boolean, phoneNumber: String, currentNumber: Boolean?, apiId: Int, apiHash: String) =
+            super.authSendCode(allowFlashcall, phoneNumber, currentNumber, apiId, apiHash)
+
+    /** Convenience method wrapping the argument with salt */
+    fun authCheckPassword(password: String): TLAuthorization {
+        val tlPassword = accountGetPassword() as? TLPassword ?:
+                throw RpcErrorException(400, "NO_PASSWORD")
+        val hash = CryptoUtils.encodePasswordHash(tlPassword.currentSalt.data, password)
+
+        @Suppress("DEPRECATION")
+        return authCheckPassword(TLBytes(hash))
+    }
+
+    @Deprecated("Use authCheckPassword for more convenience",
+                ReplaceWith("authCheckPassword(password)"))
+    override fun authCheckPassword(passwordHash: TLBytes) = super.authCheckPassword(passwordHash)
+
+    /** Convenience method wrapping the argument with TelegramApp values and casting result with good type */
+    fun <T : TLObject> initConnection(query: TLMethod<T>) = with(app) {
+        @Suppress("DEPRECATION")
+        initConnection(apiId, deviceModel,
+                       systemVersion, appVersion,
+                       systemLangCode, langPack, langCode,
+                       query)
+    }
+
+    @Deprecated("Use initConnection for more convenience", ReplaceWith("initConnection(query)"))
+    override fun <T : TLObject> initConnection(apiId: Int,
+                                               deviceModel: String,
+                                               systemVersion: String, appVersion: String,
+                                               systemLangCode: String,
+                                               langPack: String,
+                                               langCode: String,
+                                               query: TLMethod<T>?) =
+            super.initConnection(apiId, deviceModel, systemVersion, appVersion,
+                                 systemLangCode, langPack, langCode, query)
+
+    /** Convenience method wrapping the argument for a plain text message */
+    fun messagesSendMessage(peer: TLAbsInputPeer, message: String, randomId: Long) =
+            messagesSendMessage(true, false, false, false,
+                                peer, null, message, randomId,
+                                null, null)
 }
 
 /**
@@ -18,6 +79,10 @@ abstract class TelegramSyncClient : TelegramSyncApiWrapper() {
  * [io.reactivex.Single.blockingGet] or [io.reactivex.Observable.blockingIterable]
  */
 internal class TelegramSyncClientImpl(val client: TelegramClient) : TelegramSyncClient() {
+
+    override val app: TelegramApp
+        get() = client.app
+
     override fun <T : TLObject> executeRpcQuerySync(method: TLMethod<T>): T = try {
         client.executeRpcQuery(method).blockingGet()
     } catch (e: RuntimeException) {
